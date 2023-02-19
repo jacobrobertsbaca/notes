fcb1a85accf35ef63c026523bd951725481e1eab
Shounak Ray <shounak@stanford.edu>
2023-02-18 20:23:39 -0800

    fixing twinkle, playground again

---
+++ notes/Assets/Scripts/Playground.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using static SheetMusic;
+
+public class Playground
+{
+	// Use this for initialization
+	void Start()
+	{
+        SheetMusic sheet = SheetMusic.FromMIDI("twinkle-RH.mid", true);
+	}
+}
+
