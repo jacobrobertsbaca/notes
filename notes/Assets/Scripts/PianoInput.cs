8fe5d3231de052e2db9fc39663c8622ffd196239
SirHauler <realalexalvarado@gmail.com>
2023-02-19 04:58:48 -0800

    adding pianoinput

--- notes/Assets/Scripts/PianoInput.cs
+++ notes/Assets/Scripts/PianoInput.cs
@@ -1,35 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; 
+using Minis;
 
-using static SheetMusic; 
-
-/// <summary>
-/// Analagous to the KeyBoard Input Class
-/// Except its a fuckin piano boiiii
-/// </summary>
-public class NewBehaviourScript : MonoBehaviour
+sealed class PianoInput : MonoBehaviour
 {
-
-    public float Tempo { get; set; }
-
-    public SheetMusic Music { get;  }
-
-    public List<Note> NotesPlayed { get; } = new List<Note>();
-
-    public float StartTime { get; set; }
-
-    public bool gameStarted { get; set; }
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        InputSystem.onDeviceChange += (device, change) =>
+        {
+            if (change != InputDeviceChange.Added) return;
+
+            var midiDevice = device as Minis.MidiDevice;
+            if (midiDevice == null) return;
+
+            midiDevice.onWillNoteOn += (note, velocity) => {
+                // Note that you can't use note.velocity because the state
+                // hasn't been updated yet (as this is "will" event). The note
+                // object is only useful to specify the target note (note
+                // number, channel number, device name, etc.) Use the velocity
+                // argument as an input note velocity.
+                Debug.Log(string.Format(
+                    "Note On #{0} ({1}) vel:{2:0.00} ch:{3} dev:'{4}'",
+                    note.noteNumber,
+                    note.shortDisplayName,
+                    velocity,
+                    (note.device as Minis.MidiDevice)?.channel,
+                    note.device.description.product
+                ));
+            };
+
+            midiDevice.onWillNoteOff += (note) => {
+                Debug.Log(string.Format(
+                    "Note Off #{0} ({1}) ch:{2} dev:'{3}'",
+                    note.noteNumber,
+                    note.shortDisplayName,
+                    (note.device as Minis.MidiDevice)?.channel,
+                    note.device.description.product
+                ));
+            };
+        };
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log("asd");
     }
 }
