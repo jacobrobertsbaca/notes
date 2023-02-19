1378cf1282e253ac04845caf30c37bd3404473fa
SirHauler <realalexalvarado@gmail.com>
2023-02-18 23:20:22 -0800

    input updates

--- notes/Assets/Scripts/KeyboardInput.cs
+++ notes/Assets/Scripts/KeyboardInput.cs
@@ -8,31 +8,34 @@ using static Metrics;
 
 
 
-public class KeyboardInput : MonoBehaviour
+public class KeyboardInput: MonoBehaviour
 {
 
-    UnityEvent NotePlayed = new UnityEvent();
-
-    public List<Note> NotesPlayed { get; }
-    float StartTime = 0;
-
-    /// <summary>
-    /// List of all keys that are mapped to notes.
-    /// Specifically the home row of virtualpiano.net
-    /// </summary>
-    public List<KeyCode> NoteKeyCodes = new List<KeyCode>()
-    {
-        KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F,
-        KeyCode.G, KeyCode.H, KeyCode.J, KeyCode.K,
-        KeyCode.L, KeyCode.R, KeyCode.E,
-        KeyCode.Y, KeyCode.U, KeyCode.I, KeyCode.P
-    };
+    //UnityEvent NotePlayed = new UnityEvent();
 
 
-    /// <summary>
-    /// Dictionary of all of the KeyCodes from a user's keyboard to the NotePitch
-    /// </summary>
-    public IDictionary<KeyCode, NotePitch> KeyCodeToPitch = new Dictionary<KeyCode, NotePitch>() {
+    //float StartTime = 0;
+    //bool gameStarted = false;
+    //List<Note> NotesPlayed = new List<Note>();
+    //SheetMusic music;
+    //float Tempo = 0;
+
+    public float Tempo { get; set;  }
+
+    public SheetMusic Music { get;  }
+
+    public float GroundTime { get; set; }
+
+    public List<Note> NotesPlayed { get; } = new List<Note>();
+
+    public float StartTime { get; set; }
+
+    public bool gameStarted { get; set; }
+
+
+
+
+    private static readonly IReadOnlyDictionary<KeyCode, NotePitch> KeyCodeToPitch = new Dictionary<KeyCode, NotePitch>() {
         {KeyCode.A, B4},
         {KeyCode.S, C5},
         {KeyCode.D, D5},
@@ -50,36 +53,70 @@ public class KeyboardInput : MonoBehaviour
         {KeyCode.P, B5},
     };
 
-    //public KeyboardInput(Func)
-    //{
 
-    //}
+
+    private static readonly List<KeyCode> NoteKeyCodes = new List<KeyCode>()
+    {
+        KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F,
+        KeyCode.G, KeyCode.H, KeyCode.J, KeyCode.K,
+        KeyCode.L, KeyCode.R, KeyCode.E,
+        KeyCode.Y, KeyCode.U, KeyCode.I, KeyCode.P
+};
+
+
+//public KeyboardInput(SheetMusic music)
+//    {
+//        Tempo = music.Tempo;
+//        StartTime = 0;
+//        gameStarted = false;
+//    }
+
+//    public void setMusic(SheetMusic m)
+//    {
+//        music = m;
+//        Tempo = music.Tempo;
+//        Debug.Log("This is my TEMPO BABY: " + Tempo); 
+//}
+
+    /// <summary>
+    /// List of all keys that are mapped to notes.
+    /// Specifically the home row of virtualpiano.net
+    /// </summary>
+
+
+
+
+    /// <summary>
+    /// Dictionary of all of the KeyCodes from a user's keyboard to the NotePitch
+    /// </summary>
+
 
     void Update()
     {
-        // wrap this in an if statement checking for a note being pressed in general
-        foreach (KeyCode tempkey in NoteKeyCodes)
+        // if no space do not start recording input
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(tempkey))
-            {
-                StartTime = Time.time;
-            }
+            gameStarted = true;
+        }
 
-            if (Input.GetKeyUp(tempkey))
+        if (gameStarted)
+        {
+
+            foreach (KeyCode tempkey in NoteKeyCodes)
             {
-                Debug.Log("Amount of time Pressed for key: " + tempkey);
-                Debug.Log((Time.time - StartTime).ToString("00:00.00"));
-                //float noteDuration = Time.time - StartTime; 
-                NotesPlayed.Add(new Note(KeyCodeToPitch[tempkey], StartTime, Time.time - StartTime, 20));
-                foreach (Note tempNote in NotesPlayed)
+                if (Input.GetKeyDown(tempkey))
                 {
-                    Debug.Log("Here is a note you played: " + (NotePitch) tempNote.Pitch);
-                    Debug.Log("Here is the start time of the note: " + tempNote.Time);
-                    Debug.Log("Here is the beat duration: " + tempNote.Duration); 
+                    StartTime = Time.time;
                 }
 
-                // Check if the event exists and then invoke it once you are done playing the note
-                NotePlayed.Invoke(); 
+                if (Input.GetKeyUp(tempkey))
+                {
+                    Debug.Log("Amount of time Pressed for key: " + tempkey);
+                    float noteDuration = (Time.time - StartTime) * (Tempo/60);
+                    Debug.Log("This is the TEMPO" + Tempo); 
+                    NotePitch notePitch = KeyCodeToPitch[tempkey];
+                    NotesPlayed.Add(new Note(notePitch, StartTime, noteDuration, 20));
+                    Debug.Log("Duration: " + noteDuration + ", " + "Start Time: " + StartTime + ", " + "Note Pitch: " + notePitch);
             }
         }
     }
--- notes/Assets/Scripts/Metrics.cs
+++ notes/Assets/Scripts/Metrics.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static KeyboardInput;
+using static SheetMusic; 
 
 public class Metrics : MonoBehaviour
 {
+    public KeyboardInput Keyboard;
+    // TODO: Implement EVENT Listeners
 
-    // TODO: Implement EVENT Listeners so that
 
+    static string nameOfPiece = "twinkle.mid";
+    SheetMusic music = FromMIDI(nameOfPiece);
+    
     private void Awake()
     {
-        KeyboardInput keyboard = new KeyboardInput();
-        
-
+        music = music.FilterNotes(NotePitch.B3, true);
+        Keyboard.Tempo = music.Tempo;
+        // KeyboardInput keyboard = new KeyboardInput(music); // tempo required for beat calculation TODO: Have game pass this in instead of the SheetMusic? 
     }
 
+    private void Update()
+    {
+        Debug.Log($"Something cool {music.Notes.Count}"); 
+    }
 }
