2cc18376d9a0cd8e8bb43056fda443bf1fb9643d
JTRB <jacobrobertsbaca@gmail.com>
2023-02-22 22:02:46 -0800

    feat: fresh project

--- notes/Assets/Scripts/Raceway/Lane.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Lane : MonoBehaviour
-{
-    [SerializeField] private Plane plane;
-    public Plane Plane => plane;
-}
